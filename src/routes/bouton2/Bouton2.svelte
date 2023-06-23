298de0dd306e96bf756fd985d023cc12c76d737c
tantely92 <131984399+tantely92@users.noreply.github.com>
2023-06-24 00:55:05 +0200

    ajout actualités et bouton 2

--- src/routes/bouton2/Bouton2.svelte
+++ src/routes/bouton2/Bouton2.svelte
@@ -1,11 +1,86 @@
 <script>
-
-</script>
-
-<main>
-    <div>bouton 2</div>
-</main>
-
-<style>
-    
-</style>
+    /**
+   * @type {number | null}
+   */
+    let activeSection = null;
+  
+    /**
+   * @param {number | null} section
+   */
+    function toggleSection(section) {
+      activeSection = section === activeSection ? null : section;
+    }
+  </script>
+  
+  <main class="bg-gray-200 py-10">
+    <div class="container mx-auto px-4">
+      <div class="text-center">
+        <h1 class="text-4xl font-bold mb-4">Texte principal centré</h1>
+        <p class="text-lg text-gray-700">Texte plus petit en dessous</p>
+      </div>
+      <div class="flex justify-center mt-4">
+          <div>
+            <a href="/quizz">
+            <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mb-4">
+              TEST
+            </button>
+            </a>
+          </div>
+        </div>
+      <div class="flex justify-center mt-10">
+        <div class="w-1/3 px-4">
+          <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mb-4" on:click={() => toggleSection(1)}>
+            Bouton 1
+          </button>
+        </div>
+        <div class="w-1/3 px-4">
+          <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mb-4" on:click={() => toggleSection(2)}>
+            Bouton 2
+          </button>
+        </div>
+        <div class="w-1/3 px-4">
+          <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mb-4" on:click={() => toggleSection(3)}>
+            Bouton 3
+          </button>
+        </div>
+      </div>
+      {#if activeSection !== null}
+      <div class="flex justify-center mt-4">
+        <div class="w-full">
+          <div class="bg-white rounded-lg shadow-md p-6">
+            {#if activeSection === 1}
+            <div class="flex items-center">
+              <img src="path_to_image_1.jpg" alt="Image 1" class="w-16 h-16 mr-4">
+              <div>
+                <h2 class="text-xl font-bold mb-2">Titre du cadre 1</h2>
+                <p class="text-gray-700 mb-4">Description du cadre 1</p>
+              </div>
+            </div>
+            {/if}
+            {#if activeSection === 2}
+            <div class="flex items-center">
+              <img src="path_to_image_2.jpg" alt="Image 2" class="w-16 h-16 mr-4">
+              <div>
+                <h2 class="text-xl font-bold mb-2">Titre du cadre 2</h2>
+                <p class="text-gray-700 mb-4">Description du cadre 2</p>
+              </div>
+            </div>
+            {/if}
+            {#if activeSection === 3}
+            <div class="flex items-center">
+              <img src="path_to_image_3.jpg" alt="Image 3" class="w-16 h-16 mr-4">
+              <div>
+                <h2 class="text-xl font-bold mb-2">Titre du cadre 3</h2>
+                <p class="text-gray-700 mb-4">Description du cadre 3</p>
+              </div>
+            </div>
+            {/if}
+          </div>
+        </div>
+      </div>
+      
+      
+      {/if}
+    </div>
+  </main>
+  
--- src/routes/bouton4/Bouton4.svelte
+++ src/routes/bouton4/Bouton4.svelte
@@ -1,11 +1,24 @@
 <script>
+    import { onMount } from 'svelte';
+    let news = [];
+  
+    onMount(async () => {
+      const response = await fetch(`https://gnews.io/api/v4/search?q=carbone&lang=fr&token=677702bb48f6fb8683643401fdadc26b`);
 
-</script>
-
-<main>
-    <div>bouton 4</div>
-</main>
-
-<style>
-    
-</style>
+      const data = await response.json();
+      news = data.articles.slice(0, 8);
+    });
+  </script>
+  
+  <section class="news-section">
+    <h1>Les actualités récentes sur la taxe carbone</h1>
+    <div class="news-grid">
+      {#each news as item (item.url)}
+        <article class="news-item">
+          <img src={item.image} alt={item.title} />
+          <h2><a href={item.url} target="_blank">{item.title}</a></h2>
+        </article>
+      {/each}
+    </div>
+  </section>
+  
