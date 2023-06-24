2360fb8d4ec30cf00a2c9a15ef628924932023fb
Tantely92 <tantely.rana@gmail.com>
2023-06-24 21:38:03 +0200

    ajout transition page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,21 @@
 <script>
   import Banner from './Banner.svelte';
   import "carbon-components-svelte/css/g10.css";
+  import { fade } from 'svelte/transition'
+
+  export let data
 
 
 </script>
 
   <div class="maine">
     <Banner/>
-    <slot />
+    {#key data.pathname}
+      <div  in:fade={{ duration: 200, delay: 300 }}
+            out:fade={{ duration: 200 }}>
+        <slot />
+      </div>
+    {/key}
     
   </div>
   
