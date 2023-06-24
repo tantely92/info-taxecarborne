f91ce6071bbeed08f0982e0aa65e27508e6102cb
Tantely92 <tantely.rana@gmail.com>
2023-06-24 15:13:35 +0200

    split quizz info

--- src/routes/info/+page.svelte
+++ src/routes/info/+page.svelte
@@ -1,11 +1,55 @@
-<script>
+<script prerender: false>
     import Info from "./Info.svelte";
 	import Quizz from "./quizz/Quizz.svelte";
+
+	import { setContext, getContext, onMount } from 'svelte';
+	import { writable } from 'svelte/store';
+
+	const state = writable({
+		component:'quizz',
+		result:'',
+	});
+
+	let displayComponent = null;
+	onMount(() => {
+		const infoState = localStorage.getItem('infoState');
+		if (infoState){
+			const{ component, result } = JSON.parse(infoState);
+			displayComponent = component;
+		}
+		else{
+			displayComponent = 'quizz';
+		}
+
+		state.subscribe(newState => {
+			// Sauvegardez l'état dans le stockage local à chaque modification
+			console.log("new state");
+			localStorage.setItem('infoState', JSON.stringify(newState));
+		});
+		console.log('onMount ',infoState);
+  });
+
+  // Fonction pour changer l'affichage des sous-composants
+	function updateFromQuizzResult(event) {
+		displayComponent = "infos";
+		state.result = event.detail;
+		state.component = "infos";
+		localStorage.setItem('infoState', JSON.stringify(state));
+		alert(state.result);
+	}
 </script>
+
 
 <svelte:head>
 	<title>S'informer</title>
 	<meta name="description" content="Information sur la taxe carbone" />
 </svelte:head>
-<Quizz/>
-<Info/>
+<div>
+	{#if displayComponent === "quizz"}
+		<Quizz on:quizzResult={updateFromQuizzResult}/>
+	{/if}
+
+	{#if displayComponent === "infos"}
+		<Info/>
+	{/if}
+</div>
